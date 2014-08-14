cd0a1db46648b7fdfa70e4fd3614650f8ecefd64
Jeremy <jeremy@kwmassage.com>
2014-08-14 00:11:54 -0300

    blog New article - show done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,17 @@
 class ArticlesController < ApplicationController
 	def new
 	end
-	def create
-		render text: params[:article].inspect
+	def show
+		@article = Article.find(params[:id])
 	end
+	def create
+		@article = Article.new(params[:article])
+
+		@article.save
+		redirect_to @article
+	end
+	private
+	  def article_params
+	    params.require(:article).permit(:title, :text)
+	  end
 end
