003a10f6023e9544f3974854eabd29be8cae9c04
Jeremy <jeremy@kwmassage.com>
2014-08-15 02:25:43 -0300

    5.11 compleat but article update not working due to :patch not being present in rails 3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 	def new
-
+		@article = Article.new
 	end
 
 	def show
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(params[:article])
+		@article = Article.new(article_params)
 
 		if @article.save
 			redirect_to @article
@@ -21,9 +21,23 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end
+
 	private
 	  def article_params
 	    params.require(:article).permit(:title, :text)
 	  end
-	  
+
 end
