c9f00e2a107035a7b4d63100d5421c4882a3301b
Jeremy <jeremy@kwmassage.com>
2014-08-19 11:23:39 -0400

    delete all

--- app/models/article.rb
+++
@@ -1,4 +0,0 @@
-class Article < ActiveRecord::Base
-  attr_accessible :text, :title
-  validates :title, presence: true, length: {minimum: 5}
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '649f8cfd8e0c8bef71efe8b21e0d0b8a1b52867e3acb9115531fb20910d0929cc756f49f77e7b9397fce0e747682defcbd7c20ac3ec4e3df7001aae9656ed2d3'
